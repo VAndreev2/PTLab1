2c4b12f38d2f616a1c2893bbf6a506dca292de61
VAndreev2 <viktormoonwell@gmail.com>
2024-09-25 23:31:10 +0300

    Update test_CalcInfo.py

--- test/test_CalcInfo.py
+++ test/test_CalcInfo.py
@@ -1,11 +1,10 @@
 import os
 import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 import unittest
-
-sys.path.insert(1, os.path.join(sys.path[0], '../src'))
-
 from CalcInfo import CalcInfo
-
 
 class test_YamlFileReader(unittest.TestCase):
 
