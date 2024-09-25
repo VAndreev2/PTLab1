09d643cf0a35bef30097a013d821668b5c1a6fe1
VAndreev2 <viktormoonwell@gmail.com>
2024-09-25 23:43:42 +0300

    Update test_YamlFileReader.py

--- test/test_YamlFileReader.py
+++ test/test_YamlFileReader.py
@@ -5,7 +5,7 @@ import tempfile
 
 import yaml
 
-sys.path.insert(1, os.path.join(sys.path[0], '../src'))
+sys.path.insert(1, os.path.join(sys.path[0], '../src'))  # noqa: E402
 from YamlFileReader import YamlFileReader
 
 class test_YamlFileReader(unittest.TestCase):
@@ -15,8 +15,12 @@ class test_YamlFileReader(unittest.TestCase):
 
         # Ожидаемый результат
         expected_result = {
-            'Иванов Иван Иванович': [('математика', 67), ('литература', 100), ('программирование', 91)],
-            'Петров Петр Петрович': [('математика', 78), ('химия', 87), ('социология', 61)]
+            'Иванов Иван Иванович': [('математика', 67),
+                                     ('литература', 100),
+                                     ('программирование', 91)],
+            'Петров Петр Петрович': [('математика', 78),
+                                     ('химия', 87),
+                                     ('социология', 61)]
         }
         self.assertEqual(result, expected_result)
 
