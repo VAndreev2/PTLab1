b96506772d562be1b4db09bbdfe13b5983a42ee0
VAndreev2 <viktormoonwell@gmail.com>
2024-09-25 23:21:03 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ from YamlFileReader import YamlFileReader
 
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
-    parser.add_argument("-p", dest="path", type=str, default="../data/data.yml",
+    parser.add_argument("-p", dest="path", type=str, 
+                        default="../data/data.yml",
                         required=False, help="Path to datafile")
     args = parser.parse_args(args)
     return args.path
