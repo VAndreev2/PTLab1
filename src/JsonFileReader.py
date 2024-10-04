4734e8c8f70e87d559feb73f7ceda2815a3fa812
VAndreev2 <viktormoonwell@gmail.com>
2024-10-04 19:28:44 +0300

    Update and rename YamlFileReader.py to JsonFileReader.py

---
+++ src/JsonFileReader.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import json
+
+from Types import DataType
+from DataReader import DataReader
+
+
+class JsonFileReader(DataReader):
+
+    def __init__(self) -> None:
+        self.key: str = ""
+        self.students: DataType = {}
+
+    def read(self, path: str) -> DataType:
+        students: DataType = {}
+
+        with open(path, 'r', encoding='utf-8') as file:
+            # Load data from JSON file
+            data = json.load(file)
+
+        if not data:
+            raise ValueError("Файл пустой или не содержит данных.")
+
+        # Validate and parse data
+        for student, subjects in data.items():
+
+            for subj, score in subjects.items():
+                if not isinstance(subj, str) or not isinstance(score, int):
+                    raise ValueError("Предметы должны быть строками, а оценки - целыми числами.")
+                if score > 100 or score < 0:
+                    raise ValueError
+
+            students[student] = [(subj, score) for subj, score in subjects.items()]
+
+        return students
