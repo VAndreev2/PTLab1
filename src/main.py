d66c97b8b4fb201e6b2b2233ab78c3212c84fbc3
VAndreev2 <viktormoonwell@gmail.com>
2024-10-25 20:31:26 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from QuartRating import QuartRating
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
     parser.add_argument("-p", dest="path", type=str,
-                        default="../data/data.yml",
+                        default="../data/data.json",
                         required=False, help="Path to datafile")
     args = parser.parse_args(args)
     return args.path
