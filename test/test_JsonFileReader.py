50f16de25dc9c54b86baed96dda8da242c952649
VAndreev2 <viktormoonwell@gmail.com>
2024-10-26 18:44:24 +0300

    Update test_JsonFileReader.py

--- test/test_JsonFileReader.py
+++ test/test_JsonFileReader.py
@@ -4,10 +4,8 @@ import os
 import sys
 import unittest
 import tempfile
-
 sys.path.insert(1, os.path.join(sys.path[0], '../src'))
 from JsonFileReader import JsonFileReader # noqa: E402
-
 
 class test_JsonFileReader(unittest.TestCase):
 
