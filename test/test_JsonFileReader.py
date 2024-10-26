f264d2cf1cadbd76c5ff61f3a978dd536be74118
VAndreev2 <viktormoonwell@gmail.com>
2024-10-26 18:22:03 +0300

    Update test_JsonFileReader.py

--- test/test_JsonFileReader.py
+++ test/test_JsonFileReader.py
@@ -1,21 +1,43 @@
+import argparse
+import json
 import os
 import sys
 import unittest
 import tempfile
-import json
 
-sys.path.insert(1, os.path.join(sys.path[0], '../src'))  # noqa: E402
+sys.path.insert(1, os.path.join(sys.path[0], '../src'))
 from JsonFileReader import JsonFileReader
 
 
 class test_JsonFileReader(unittest.TestCase):
 
-    def test_read(self):
-        current_dir = os.path.dirname(__file__)
-        file_path = os.path.join(current_dir, '../data/data.json')
-        result = JsonFileReader().read(file_path)
+    @staticmethod
+    def get_path_from_arguments(args) -> str:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('filename', nargs='?', default='../data/data.json')
+        parser.add_argument('unittest_args', nargs='*')
+        args = parser.parse_args(args)
+        sys.argv[1:] = args.unittest_args
+        return args.filename
 
-        # Ожидаемый результат
+    def create_temp_file(self, data):
+        with tempfile.NamedTemporaryFile('w+', suffix='.json',
+                                         delete=False,
+                                         encoding='utf-8') as temp_file:
+            json.dump(data, temp_file, ensure_ascii=False, indent=4)
+            temp_file_name = temp_file.name  # Сохраняем имя файла
+            return temp_file_name
+
+    def reader_for_test(self, temp_file_name):
+        try:
+            with self.assertRaises(ValueError):
+                JsonFileReader().read(temp_file_name)
+        finally:
+            # Удаление временного файла вручную после использования
+            if os.path.exists(temp_file_name):
+                os.remove(temp_file_name)
+
+    def test_read(self):
         expected_result = {
             'Иванов Иван Иванович': [('математика', 67),
                                      ('литература', 100),
@@ -24,77 +46,53 @@ class test_JsonFileReader(unittest.TestCase):
                                      ('химия', 87),
                                      ('социология', 61)]
         }
-        self.assertEqual(result, expected_result)
+        self.assertEqual(
+            JsonFileReader().read(
+                path=self.get_path_from_arguments(sys.argv[1:])
+            ),
+            expected_result
+        )
 
     def test_desc_type(self):
 
         data = {
-                'Иванов Иван Иванович': {
-                    'математика': '123',
-                    'литература': 100,
-                    'программирование': 91
-                }
+            'Иванов Иван Иванович': {
+                'математика': '123',
+                'литература': 100,
+                'программирование': 91
+            }
         }
         # Создание временного файла и запись данных
-        with tempfile.NamedTemporaryFile('w+',
-                                         suffix='.json',
-                                         delete=False,
-                                         encoding='utf-8') as temp_file:
-            json.dump(data, temp_file, ensure_ascii=False, indent=4)
-            temp_file_name = temp_file.name  # Сохраняем имя файла
-        try:
-            with self.assertRaises(ValueError):
-                JsonFileReader().read(temp_file_name)
-        finally:
-            # Удаление временного файла вручную после использования
-            if os.path.exists(temp_file_name):
-                os.remove(temp_file_name)
+        temp_file = self.create_temp_file(data)
+        self.reader_for_test(temp_file)
 
     def test_number_negative(self):
 
         data = {
-                "Иванов Иван Иванович": {
-                    'математика': 67,
-                    'литература': -100,
-                    'программирование': 91
-                }
+            "Иванов Иван Иванович": {
+                'математика': 67,
+                'литература': -100,
+                'программирование': 91
+            }
         }
 
         # Создание временного файла и запись данных
-        with tempfile.NamedTemporaryFile('w+',
-                                         suffix='.json',
-                                         delete=False,
-                                         encoding='utf-8') as temp_file:
-            json.dump(data, temp_file, ensure_ascii=False, indent=4)
-            temp_file_name = temp_file.name  # Сохраняем имя файла
-        try:
-            with self.assertRaises(ValueError):
-                JsonFileReader().read(temp_file_name)
-        finally:
-            # Удаление временного файла вручную после использования
-            if os.path.exists(temp_file_name):
-                os.remove(temp_file_name)
+        temp_file = self.create_temp_file(data)
+        self.reader_for_test(temp_file)
 
     def test_number_biggest(self):
         data = {
-                "Иванов Иван Иванович": {
-                    'математика': 101,
-                    'литература': 100,
-                    'программирование': 91
-                }
+            "Иванов Иван Иванович": {
+                'математика': 101,
+                'литература': 100,
+                'программирование': 91
+            }
         }
 
         # Создание временного файла и запись данных
-        with tempfile.NamedTemporaryFile('w+',
-                                         suffix='.json',
-                                         delete=False,
-                                         encoding='utf-8') as temp_file:
-            json.dump(data, temp_file, ensure_ascii=False, indent=4)
-            temp_file_name = temp_file.name  # Сохраняем имя файла
-        try:
-            with self.assertRaises(ValueError):
-                JsonFileReader().read(temp_file_name)
-        finally:
-            # Удаление временного файла вручную после использования
-            if os.path.exists(temp_file_name):
-                os.remove(temp_file_name)
+        temp_file = self.create_temp_file(data)
+        self.reader_for_test(temp_file)
+
+
+if __name__ == "__main__":
+    unittest.main(argv=[sys.argv[0]], exit=False)
